820df6dea3592d11045ff64a3fc85e86986b607b
miltinn <augusto.milton99@gmail.com>
2022-11-18 19:16:54 -0300

    Removidos logs de teste do exercício 2.

--- Primeiras páginas interativas com JavaScript/Primeiros passos com JavaScript/02. Condicionais/segundo-desafio.js
+++ Primeiras páginas interativas com JavaScript/Primeiros passos com JavaScript/02. Condicionais/segundo-desafio.js
@@ -5,14 +5,13 @@ const gastoKm = 0.1                 //consumo de combustivel (litros por km)
 
 function custoViagem(combustivel) {
     combustivel = String(combustivel).toLowerCase();
-    console.log(combustivel)
     if (combustivel === "etanol") {
-        return distancia * gastoKm * precoEtanol;
+        return (distancia * gastoKm * precoEtanol).toFixed(2);
     } else if (combustivel === "gasolina") {
-        return distancia * gastoKm * precoGasolina;
+        return (distancia * gastoKm * precoGasolina).toFixed(2);
     } else {
         return "tipo de combustível não encontrado"
     }
 }
-console.log(custoViagem("etanol"));
+console.log(custoViagem("etaNol"));
 
